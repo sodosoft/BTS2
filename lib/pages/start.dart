b6dc8794fa12acc52b25ab26899f197958d86471
sodosoft <hidessam80@gmail.com>
2023-01-30 02:04:58 +0900

    bangtong

--- lib/pages/start.dart
+++ lib/pages/start.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:bangtong/api/api.dart';
+import 'package:bangtong/pages/DetailDriver.dart';
 import 'package:bangtong/pages/editScreen.dart';
 import 'package:flutter/material.dart'; //flutter의 package를 가져오는 코드 반드시 필요
 import 'package:fluttertoast/fluttertoast.dart';
@@ -204,7 +205,7 @@ class _MyAppState extends State<startArea> {
                                     context,
                                     MaterialPageRoute(
                                         builder: (context) =>
-                                            DetailPage(snapshot.data[index])));
+                                            DetailPageDriver(snapshot.data[index])));
                               },
                             ),
                           );
@@ -226,99 +227,4 @@ class _MyAppState extends State<startArea> {
   }
 }
 
-class DetailPage extends StatelessWidget {
-  final OrderData postData;
-  bool _visble = false;
-
-  DetailPage(this.postData); // 생성자를 통해서 입력변수 받기
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: Text(DisplayString.displayArea(postData.startArea) +
-            " >> " +
-            DisplayString.displayArea(postData.endArea)),
-      ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-        height: double.maxFinite,
-        child:Column(
-          children:
-          [
-            Text('상차지: ' + postData.startArea.toString()  + '\n' +
-                '하차지: ' + postData.endArea.toString()  + '\n' +
-                '상차일시: ' + DateFormat("yyyy년 MM월 dd일 HH시 mm분").format(DateTime.parse(postData.startDateTime)) + '\n' +
-                '하차일시: ' + DateFormat("yyyy년 MM월 dd일 HH시 mm분").format(DateTime.parse(postData.endDateTime)) + '\n' +
-                '운반비: ￦' + postData.cost + "원"  + '\n' +
-                '지불방식: ' + postData.payMethod.toString()  + '\n' +
-                '차종: ' + postData.carKind.toString()  + '\n' +
-                '품목: ' + postData.product.toString()  + '\n' +
-                '등급: ' + postData.grade.toString()  + '\n' +
-                '상차방법: ' + postData.startMethod.toString()  + '\n' +
-                '차량번호: ' + postData.userCarNo.toString()
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                child: Text('오더 잡기'),
-                onPressed: (){
-                  // 화주한테 차번호 SMS 보내기
-                  // orderYN Y로 업데이트
-                  // 타이머 10분
-                  _visble = true;
-                },
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Visibility(
-              visible: _visble,
-              child:
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-
-                  child: Text('전화 걸기'),
-                  onPressed: (){
-                    // 화주한테 전화 걸기
-
-                  },
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Visibility(
-              visible: _visble,
-              child:
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-
-                  child: Text('취소'),
-                  onPressed: (){
-                    // 취소
-                    // orderYN N으로 업데이트
-                    // 캔슬 횟수 추가(캔슬 횟수 하루에 3번 제한)
-                    // 화면 닫음
-                  },
-                ),
-              ),
-            ),
-        ],),
-      ),
-      //body: Text(postData.content),
-    );
-  }
-}
 
